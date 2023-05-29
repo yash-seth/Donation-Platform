c0803250bc8bfb2dd9cfff22d7ca33167e341204
Yash Seth <yashseth2002@gmail.com>
2023-05-30 00:49:36 +0530

    feat(homepage): setup default '/' path routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import About from './Components/About/About';
 import Navbar from './Components/Navbar/Navbar';
 import Contact from './Components/Contact/Contact';
 import Donations from './Components/Donations/Donations';
+import Homepage from './Components/Homepage/Homepage';
 
 import {
   BrowserRouter as Router,
@@ -15,7 +16,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        {/* <Route exact path="/" element={}></Route> */}
+        <Route exact path="/" element={<Homepage />}></Route>
         <Route exact path="/about-us" element={<About />}></Route>
         <Route exact path="/contact-us" element={<Contact />}></Route>
         <Route exact path="/donations" element={<Donations />}></Route>
