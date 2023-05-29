bfdb84d522b32ae2c9936eb860fbd771a11edcd3
Yash Seth <yashseth2002@gmail.com>
2023-05-30 01:06:37 +0530

    feat(homepage): add carousel

--- frontend/src/Components/Homepage/Homepage.jsx
+++ frontend/src/Components/Homepage/Homepage.jsx
@@ -7,21 +7,21 @@ import "./Homepage.css"
 function Homepage() {
   return (
     <div className="homepage">
-      <div style={{ display: "block", width: "60vw", padding: 30 }}>
-        <h4>React-Bootstrap Carousel Component</h4>
+      <div className="carousel">
+        {/* <h4>React-Bootstrap Carousel Component</h4> */}
         <Carousel>
           <Carousel.Item interval={1500}>
             <img className="d-block w-100" src={childrenPlaying} alt="One" />
             <Carousel.Caption>
-              <h3>Label for first slide</h3>
-              <p>Sample Text for Image One</p>
+              <h3>Football Players</h3>
+              <p>Bhandup, Mumbai</p>
             </Carousel.Caption>
           </Carousel.Item>
-          <Carousel.Item interval={500}>
+          <Carousel.Item interval={1500}>
             <img className="d-block w-100" src={childrenPlaying} alt="Two" />
             <Carousel.Caption>
-              <h3>Label for second slide</h3>
-              <p>Sample Text for Image Two</p>
+              <h3>In rain (in pic)</h3>
+              <p>Two mulund warriors players</p>
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
