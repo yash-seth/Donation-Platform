b023b5729c837bda20d7b7c6b6ce82a1f9705b51
Yash Seth <yashseth2002@gmail.com>
2023-05-30 11:06:26 +0530

    fix(homepage): fix carousel resizing issue

--- frontend/src/Components/Homepage/Homepage.jsx
+++ frontend/src/Components/Homepage/Homepage.jsx
@@ -1,30 +1,32 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Carousel from "react-bootstrap/Carousel";
-import childrenPlaying from "../../Assets/children_playing.jpg"
-import "./Homepage.css"
+import childrenPlaying from "../../Assets/children_playing.jpg";
+import "./Homepage.css";
 
 function Homepage() {
   return (
     <div className="homepage">
-      <div className="carousel">
-        {/* <h4>React-Bootstrap Carousel Component</h4> */}
-        <Carousel>
-          <Carousel.Item interval={1500}>
-            <img className="d-block w-100" src={childrenPlaying} alt="One" />
-            <Carousel.Caption>
-              <h3>Football Players</h3>
-              <p>Bhandup, Mumbai</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item interval={1500}>
-            <img className="d-block w-100" src={childrenPlaying} alt="Two" />
-            <Carousel.Caption>
-              <h3>In rain (in pic)</h3>
-              <p>Two mulund warriors players</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
+      <div className="carousel-container">
+        <div className="carousel">
+          {/* <h4>React-Bootstrap Carousel Component</h4> */}
+          <Carousel>
+            <Carousel.Item interval={3000}>
+              <img className="d-block w-100" src={childrenPlaying} alt="One" width={"auto"} height={"100%"}/>
+              <Carousel.Caption>
+                <h3 id="photo-header">Football Players</h3>
+                <p id="photo-caption">Bhandup, Mumbai</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={3000}>
+              <img className="d-block w-100" src={childrenPlaying} alt="Two" width={"auto"} height={"100%"}/>
+              <Carousel.Caption>
+                <h3 id="photo-header">In rain (in pic)</h3>
+                <p id="photo-caption">Two mulund warriors players</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </div>
       </div>
     </div>
   );
