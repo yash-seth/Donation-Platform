66a373c6c53ec444abfc191dd3525011ff7c1a77
Yash Seth <yashseth2002@gmail.com>
2023-05-30 12:44:55 +0530

    feat(donations): add transit orders view

--- frontend/src/Components/Donations/Donations.jsx
+++ frontend/src/Components/Donations/Donations.jsx
@@ -4,6 +4,9 @@ import { completedOrders } from "../../Data";
 
 function Donations() {
   const [dashboardView, setDashboardView] = useState("completed");
+  function sendReminder(e) {
+    console.log(e.orderID)
+  }
   return (
     <div className="donations">
       <div className="sidemenu">
@@ -52,7 +55,37 @@ function Donations() {
           </table>
         </div>
       ) : dashboardView === "transit" ? (
-        <div>Transit Orders</div>
+        <div className="completedOrders">
+          <div className="donations-header">
+            <h1>Transit Orders</h1>
+          </div>
+          <table className="styled-table">
+            <thead>
+              <tr>
+                <th>Order ID</th>
+                {/* <th>Status</th> */}
+                <th>Name</th>
+                <th>Contact Number</th>
+                <th>Email Address</th>
+                <th>Initiated Date</th>
+                <th>Action</th>
+            </tr>
+            </thead>
+            <tbody>
+              {transitOrders.map((order) => {return (
+                <tr id={order.orderID}>
+                  <td>{order.orderID}</td>
+                  <td>{order.name}</td>
+                  {/* <td>{order.status}</td> */}
+                  <td>{order.contact}</td>
+                  <td>{order.email}</td>
+                  <td>{order.initiatedDate}</td>
+                  <td><button onClick={(e) => sendReminder(e)}>Remind</button></td>
+                </tr>
+              )})}
+            </tbody>
+          </table>
+        </div>
       ) : dashboardView === "new" ? (
         <div>New Order</div>
       ) : (
