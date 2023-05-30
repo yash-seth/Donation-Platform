b2337ad557ef60b17a692f5759b1693e7a93a8cf
Yash Seth <yashseth2002@gmail.com>
2023-05-30 12:42:02 +0530

    chore(data): add date attribute to dummy data

--- frontend/src/Data.js
+++ frontend/src/Data.js
@@ -1,15 +1,15 @@
 let completedOrders = [
-    {orderID: 0, status: "completed", name: "Yash Seth", contact: "8108015244", email: "xyz@email.com"},
-    {orderID: 1, status: "completed", name: "abc", contact: "123456", email: "abc@email.com"},
-    {orderID: 2, status: "completed", name: "xyz", contact: "444444", email: "xyz@email.com"},
-    {orderID: 3, status: "completed", name: "pqr", contact: "555555", email: "pqr@email.com"},
+    {orderID: 0, status: "completed", name: "Yash Seth", contact: "8108015244", email: "xyz@email.com", completedDate: "19/04/2023"},
+    {orderID: 1, status: "completed", name: "abc", contact: "123456", email: "abc@email.com", completedDate: "19/12/2022"},
+    {orderID: 2, status: "completed", name: "xyz", contact: "444444", email: "xyz@email.com", completedDate: "02/09/2022"},
+    {orderID: 3, status: "completed", name: "pqr", contact: "555555", email: "pqr@email.com", completedDate: "24/01/2023"},
 ]
 
 let transitOrders = [
-    {orderID: 0, status: "transit", name: "Yash Seth", contact: "8108015244", email: "xyz@email.com"},
-    {orderID: 1, status: "transit", name: "abc", contact: "123456", email: "abc@email.com"},
-    {orderID: 2, status: "transit", name: "xyz", contact: "444444", email: "xyz@email.com"},
-    {orderID: 3, status: "transit", name: "pqr", contact: "555555", email: "pqr@email.com"},
+    {orderID: 0, status: "transit", name: "Yash Seth", contact: "8108015244", email: "xyz@email.com", initiatedDate: "06/04/2023"},
+    {orderID: 1, status: "transit", name: "abc", contact: "123456", email: "abc@email.com", initiatedDate: "03/02/2023"},
+    {orderID: 2, status: "transit", name: "xyz", contact: "444444", email: "xyz@email.com", initiatedDate: "02/03/2023"},
+    {orderID: 3, status: "transit", name: "pqr", contact: "555555", email: "pqr@email.com", initiatedDate: "12/01/2023"},
 ]
 
 export { 
