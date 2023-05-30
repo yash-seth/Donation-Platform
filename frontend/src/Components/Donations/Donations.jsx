f7601539fd1884980012c2a87528401705af7f9d
Yash Seth <yashseth2002@gmail.com>
2023-05-30 11:55:06 +0530

    feat(donations): basic nav and structure for donations

--- frontend/src/Components/Donations/Donations.jsx
+++ frontend/src/Components/Donations/Donations.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { React, useState } from 'react'
+import "./Donations.css"
+import { completedOrders } from '../../Data'
 
 function Donations() {
+  const [dashboardView, setDashboardView] = useState("completed")
   return (
-    <div>Donations</div>
+    <div className="donations">
+      <div className='sidemenu'>
+        <button className='sidemenu-btns' onClick={() => setDashboardView("completed")}>Completed Donations</button>
+        <button className='sidemenu-btns' onClick={() => setDashboardView("transit")}>In transit</button>
+        <button className='sidemenu-btns' onClick={() => setDashboardView("new")}>New Donation</button>
+      </div>
+      {dashboardView === "completed" ? (<div>Completed Orders</div>) 
+      :
+      dashboardView === "transit" ? (<div>Transit Orders</div>) 
+      :
+      dashboardView === "new" ? (<div>New Order</div>)
+      : 
+      (<div>Completed Orders</div>)}  
+    </div>
   )
 }
 
