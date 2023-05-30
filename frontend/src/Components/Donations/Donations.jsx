3c5a59ba1f642c5db9d65703920e63f3b0adf404
Yash Seth <yashseth2002@gmail.com>
2023-05-30 12:12:42 +0530

    feat(donations): completed orders view code

--- frontend/src/Components/Donations/Donations.jsx
+++ frontend/src/Components/Donations/Donations.jsx
@@ -1,25 +1,65 @@
-import { React, useState } from 'react'
-import "./Donations.css"
-import { completedOrders } from '../../Data'
+import { React, useState } from "react";
+import "./Donations.css";
+import { completedOrders } from "../../Data";
 
 function Donations() {
-  const [dashboardView, setDashboardView] = useState("completed")
+  const [dashboardView, setDashboardView] = useState("completed");
   return (
     <div className="donations">
-      <div className='sidemenu'>
-        <button className='sidemenu-btns' onClick={() => setDashboardView("completed")}>Completed Donations</button>
-        <button className='sidemenu-btns' onClick={() => setDashboardView("transit")}>In transit</button>
-        <button className='sidemenu-btns' onClick={() => setDashboardView("new")}>New Donation</button>
+      <div className="sidemenu">
+        <button
+          className="sidemenu-btns"
+          onClick={() => setDashboardView("completed")}
+        >
+          Completed Donations
+        </button>
+        <button
+          className="sidemenu-btns"
+          onClick={() => setDashboardView("transit")}
+        >
+          In transit
+        </button>
+        <button
+          className="sidemenu-btns"
+          onClick={() => setDashboardView("new")}
+        >
+          New Donation
+        </button>
       </div>
-      {dashboardView === "completed" ? (<div>Completed Orders</div>) 
-      :
-      dashboardView === "transit" ? (<div>Transit Orders</div>) 
-      :
-      dashboardView === "new" ? (<div>New Order</div>)
-      : 
-      (<div>Completed Orders</div>)}  
+      {dashboardView === "completed" ? (
+        <div className="completedOrders">
+          <table className="styled-table">
+            <thead>
+              <tr>
+                <th>Order ID</th>
+                {/* <th>Status</th> */}
+                <th>Name</th>
+                <th>Contact Number</th>
+                <th>Contact Number</th>
+            </tr>
+            </thead>
+            <tbody>
+              {completedOrders.map((order) => {return (
+                <tr id={order.orderID}>
+                  <td>{order.orderID}</td>
+                  <td>{order.name}</td>
+                  {/* <td>{order.status}</td> */}
+                  <td>{order.contact}</td>
+                  <td>{order.email}</td>
+                </tr>
+              )})}
+            </tbody>
+          </table>
+        </div>
+      ) : dashboardView === "transit" ? (
+        <div>Transit Orders</div>
+      ) : dashboardView === "new" ? (
+        <div>New Order</div>
+      ) : (
+        <div>Completed Orders</div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Donations
+export default Donations;
--- frontend/src/Data.js
+++ frontend/src/Data.js
@@ -1,8 +1,8 @@
 let completedOrders = [
-    {id: 0, status: "completed", name: "Yash Seth", contact: "8108015244", email: "xyz@email.com"},
-    {id: 1, status: "completed", name: "abc", contact: "123456", email: "abc@email.com"},
-    {id: 2, status: "completed", name: "xyz", contact: "444444", email: "xyz@email.com"},
-    {id: 3, status: "completed", name: "pqr", contact: "555555", email: "pqr@email.com"},
+    {orderID: 0, status: "completed", name: "Yash Seth", contact: "8108015244", email: "xyz@email.com"},
+    {orderID: 1, status: "completed", name: "abc", contact: "123456", email: "abc@email.com"},
+    {orderID: 2, status: "completed", name: "xyz", contact: "444444", email: "xyz@email.com"},
+    {orderID: 3, status: "completed", name: "pqr", contact: "555555", email: "pqr@email.com"},
 ]
 
 export { completedOrders }
