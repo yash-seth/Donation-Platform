fd1fa5295cbe9ff5c621311d755309c58a1518e8
Yash Seth <yashseth2002@gmail.com>
2023-05-31 16:06:35 +0530

    feat(backend): add mongodb connection

---
+++ backend/index.js
@@ -0,0 +1,64 @@
+// To connect with your mongoDB database
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config()
+
+mongoose.connect('mongodb://127.0.0.1:27017/' + process.env.DB_NAME)
+ 
+// Schema for users of app
+const orderSchema = new mongoose.Schema({
+    name: String,
+    contact: String,
+    email: String,
+    date: String,
+    status: String
+});
+
+// creating the model - think of it as a class
+const Order = mongoose.model('orders', orderSchema);
+ 
+// For backend and express
+const express = require('express');
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+ 
+    res.send("App is Working");
+    // You can check backend is working or not by
+    // entering http://localhost:5000
+     
+    // If you see App is working means
+    // backend working properly
+});
+ 
+app.post("/add-order", async (req, res) => {
+    try {
+        console.log(req.body)
+        const order = new Order({
+            name: "abc",
+            contact: "abc",
+            email: "abc",
+            date: "abc",
+            status: "abc"
+        });
+        let result = await order.save();
+        result = result.toObject();
+        if (result) {
+            res.send({msg: "Order added successfully!"});
+            console.log(result);
+        } else {
+            res.send({msg: "There was an error trying to add the order, please try again."})
+        }
+    } catch (e) {
+        res.send("Something Went Wrong. Please try again. " + e);
+    }
+});
+
+app.listen(5000, () => {
+    console.log("Server is running at port 5000")
+});
