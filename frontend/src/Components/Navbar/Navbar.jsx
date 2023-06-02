b5fdec518417378f331c08044b2a51e586dd30f8
Yash Seth <yashseth2002@gmail.com>
2023-06-02 12:28:21 +0530

    feat(auth): add auth for log in and log out

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,9 @@ import { useState } from 'react'
 import "./Navbar.css"
 import { Link } from 'react-router-dom';
 import HamburgerMenu from "../../Assets/burger-menu-right-svgrepo-com.svg"
+import { useAuth0 } from "@auth0/auth0-react";
+import LogoutButton from '../Login/LogoutButton/LogoutButton';
+import LoginButton from '../Login/LoginButton/LoginButton';
   
 
 function Navbar() {
@@ -10,6 +13,7 @@ function Navbar() {
   const toggleNavbarMenu = () => {
     menuState === 'navbar-menu-mobile' ? setMenuState("navbar-menu-mobile-visible") : setMenuState("navbar-menu-mobile")
   }
+  const { isAuthenticated } = useAuth0();
   return (
     <>
     <div className='navbar'>
@@ -19,6 +23,7 @@ function Navbar() {
             <Link to="/about-us" style={{ textDecoration: 'none', color: 'black' }}><button className='nav-controls-btns'>About Us</button></Link>
             <Link to="/contact-us" style={{ textDecoration: 'none', color: 'black' }}><button className='nav-controls-btns'>Contact Us</button></Link>
             <Link to="/donations" style={{ textDecoration: 'none', color: 'black' }}><button className='nav-controls-btns'>Donate</button></Link>
+            {isAuthenticated ? <LogoutButton /> : <LoginButton />}
         </div>
         <button id="hamburger-menu-mobile" onClick={toggleNavbarMenu}><img src={HamburgerMenu} alt="hamburger menu" height={"20vh"}/></button>
     </div>
@@ -28,6 +33,7 @@ function Navbar() {
         <Link to="/about-us" style={{ color: 'black' }}><button className='nav-controls-btns' onClick={toggleNavbarMenu}>About Us</button></Link>
         <Link to="/contact-us" style={{ color: 'black' }}><button className='nav-controls-btns' onClick={toggleNavbarMenu}>Contact Us</button></Link>
         <Link to="/donations" style={{ color: 'black' }}><button className='nav-controls-btns' onClick={toggleNavbarMenu}>Donate</button></Link>
+        {isAuthenticated ? <LogoutButton /> : <LoginButton />}
       </div>
     </div>
 </>
