61c11a1be69bb790f7422de231e19c6fe78a0cdc
Yash Seth <yashseth2002@gmail.com>
2023-06-02 14:48:13 +0530

    style: change naming of login btn

--- frontend/src/Components/Login/LoginButton/LoginButton.jsx
+++ frontend/src/Components/Login/LoginButton/LoginButton.jsx
@@ -4,7 +4,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()} className='nav-controls-btns'>Log In</button>;
+  return <button onClick={() => loginWithRedirect()} className='nav-controls-btns'>Admin Log In</button>;
 };
 
 export default LoginButton;
