5308e504136c97193dc31e0a2d015de57ec9daa2
Yash Seth <yashseth2002@gmail.com>
2023-06-10 23:23:13 +0530

    feat: add support for spinner for auth is loading

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import HamburgerMenu from "../../Assets/burger-menu-right-svgrepo-com.svg"
 import { useAuth0 } from "@auth0/auth0-react";
 import LogoutButton from '../Login/LogoutButton/LogoutButton';
 import LoginButton from '../Login/LoginButton/LoginButton';
+import { TailSpin } from  'react-loader-spinner'
   
 
 function Navbar() {
@@ -13,7 +14,7 @@ function Navbar() {
   const toggleNavbarMenu = () => {
     menuState === 'navbar-menu-mobile' ? setMenuState("navbar-menu-mobile-visible") : setMenuState("navbar-menu-mobile")
   }
-  const { isAuthenticated } = useAuth0();
+  const { isAuthenticated, isLoading } = useAuth0();
   return (
     <>
     <div className='navbar'>
@@ -23,7 +24,16 @@ function Navbar() {
             <Link to="/about-us" style={{ textDecoration: 'none', color: 'black' }}><button className='nav-controls-btns'>About Us</button></Link>
             <Link to="/contact-us" style={{ textDecoration: 'none', color: 'black' }}><button className='nav-controls-btns'>Contact Us</button></Link>
             <Link to="/donations" style={{ textDecoration: 'none', color: 'black' }}><button className='nav-controls-btns'>Donate</button></Link>
-            {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+            {isAuthenticated ?  <LogoutButton /> : isLoading ? <TailSpin
+                                                                  height="50"
+                                                                  width="50"
+                                                                  color="#4fa94d"
+                                                                  ariaLabel="tail-spin-loading"
+                                                                  radius="1"
+                                                                  wrapperStyle={{}}
+                                                                  wrapperClass=""
+                                                                  visible={true}
+                                                                /> :<LoginButton />}
         </div>
         <button id="hamburger-menu-mobile" onClick={toggleNavbarMenu}><img src={HamburgerMenu} alt="hamburger menu" height={"20vh"}/></button>
     </div>
