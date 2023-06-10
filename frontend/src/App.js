d22de24fa2ef34109706a02b6083226c7d2f8f09
Yash Seth <yashseth2002@gmail.com>
2023-06-10 23:30:21 +0530

    feat: add support for content spinner when loading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,39 @@
-import './App.css';
-import About from './Components/About/About';
-import Navbar from './Components/Navbar/Navbar';
-import Contact from './Components/Contact/Contact';
-import Donations from './Components/Donations/Donations';
-import Homepage from './Components/Homepage/Homepage';
+import "./App.css";
+import About from "./Components/About/About";
+import Navbar from "./Components/Navbar/Navbar";
+import Contact from "./Components/Contact/Contact";
+import Donations from "./Components/Donations/Donations";
+import Homepage from "./Components/Homepage/Homepage";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Triangle } from "react-loader-spinner";
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const { isAuthenticated, isLoading } = useAuth0();
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Homepage />}></Route>
-        <Route exact path="/about-us" element={<About />}></Route>
-        <Route exact path="/contact-us" element={<Contact />}></Route>
-        <Route exact path="/donations" element={<Donations />}></Route>
-      </Routes>
+      {isLoading ? (
+        <div id="content-spinner">
+          <Triangle
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="triangle-loading"
+            wrapperStyle={{}}
+            wrapperClassName=""
+            visible={true}
+          />
+        </div>
+      ) : (
+        <Routes>
+          <Route exact path="/" element={<Homepage />}></Route>
+          <Route exact path="/about-us" element={<About />}></Route>
+          <Route exact path="/contact-us" element={<Contact />}></Route>
+          <Route exact path="/donations" element={<Donations />}></Route>
+        </Routes>
+      )}
     </Router>
   );
 }
